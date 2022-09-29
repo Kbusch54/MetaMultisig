669d7943b85cda39ad8144d4d70a9768ebb519f6
Kbusch54 <kevinbusch@comcast.net>
2022-09-29 16:36:38 -0400

    First init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="text-red-500 text-3xl">
+      <p className="text-blue-500 ml-12">hi </p>
+    </div>
+  );
+};
+
+export default Home;
